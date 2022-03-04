6962cd6e1bce8c96b7a83a4a1f850b17e9830673
Aditya Pandey <aditya@autospace.co>
2022-03-04 06:58:56 +0000

    Fix repo name and repo  owner

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,9 @@ if [ -z "$PR_NUMBER" ]; then
   exit 1
 fi
 
-REPO_OWNER=$(jq -r .event.base.repo.owner /github/workflow/event.json)
-REPO_NAME=$(jq -r .event.base.repo.name /github/workflow/event.json)
+REPO_OWNER=$(jq -r .repository.owner.login /github/workflow/event.json)
+REPO_NAME=$(jq -r .repository.name /github/workflow/event.json)
 EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
-
-echo $(jq . /github/workflow/event.json)
-exit 1
 
 # Default the Fly app name to pr-{number}-{repo_owner}-{repo_name}
 app="${INPUT_NAME:-pr-$PR_NUMBER-$REPO_OWNER-$REPO_NAME}"
